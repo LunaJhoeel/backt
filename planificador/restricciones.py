0221341e0407918421d6e9b359874bc518cbb21a
Jhoeel Luna <jhoeel.luna@auna.org>
2025-08-09 18:49:52 -0500

    Agregados inline comments de restricciones

--- planificador/restricciones.py
+++ planificador/restricciones.py
@@ -38,24 +38,30 @@ def es_candidata(estado: "EstadoPlan", enfermera: Enfermera, dia: Dia, turno: Tu
     """
     Verifica disponibilidad y reglas para (enfermera, dia, turno)
     """
+    # Horarios declarados que les convienen a las licenciadas
     if turno not in estado.dominio_disponibilidad[(enfermera, dia)]:
         return False
-
+    
+    # Maximo un turno por persona y por dia
     if enfermera in estado.asignadas_por_dia[dia]:
         return False
 
+    # Maximo turnos por semana
     if estado.total_turnos_por_enfermera[enfermera] >= estado.max_turnos_semana:
         return False
 
+    # Maximo noches por semana
     if turno == "N" and estado.noches_por_enfermera[enfermera] >= estado.max_noches_semana:
         return False
 
+    # Si trabajo noche anterior, no puede tomar siguiente manana
     indice_actual: int = estado.indice_por_dia[dia]
     if indice_actual > 0 and turno == "M":
         dia_anterior: Dia = estado.dias[indice_actual - 1]
         if enfermera in estado.plan[dia_anterior]["N"]:
             return False
 
+    # Maximos dias consecutivos
     if not respeta_max_consecutivos(estado, enfermera, dia):
         return False
 
